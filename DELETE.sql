98e8bf6c23615b0ec66eca593403484265182436
Flavio Franco <flavio.win@gmail.com>
2021-07-21 20:27:35 -0300

    final exercise

--- DELETE.sql
+++ DELETE.sql
@@ -15,3 +15,16 @@ WHERE actor_id IN (8, 103, 181);
 DELETE FROM sakila.actor
 WHERE first_name = 'MATTHEW';
 
+--3- Exclua da tabela film_text todos os registros que possuem a palavra "saga" em suas descrições.
+SELECT * FROM sakila.film_text
+WHERE description LIKE '%saga%';
+
+DELETE FROM sakila.film_text
+WHERE description LIKE '%saga%';
+
+--4- Apague da maneira mais performática possível todos os registros das tabelas film_actor e film_category.
+SELECT * FROM sakila.film_actor;
+TRUNCATE sakila.film_actor;
+SELECT * FROM sakila.film_category;
+TRUNCATE sakila.film_category;
+
