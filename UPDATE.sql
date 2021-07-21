ddc61a37ba7fc54a4df7be19834d4ccb4aa15624
Flavio Franco <flavio.win@gmail.com>
2021-07-21 16:45:51 -0300

    update 1

---
+++ UPDATE.sql
@@ -0,0 +1,4 @@
+/* 1- Atualize o primeiro nome de todas as pessoas da tabela sakila.actor que possuem o primeiro nome "JULIA" para "JULES". */
+UPDATE sakila.actor
+SET first_name = 'JULIES'
+WHERE first_name = 'JULIA';
