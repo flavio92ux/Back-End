c12102955e14c19342c973ebf6c40177d135eb14
Flavio Franco <flavio.win@gmail.com>
2021-07-21 16:49:39 -0300

    update 2

--- UPDATE.sql
+++ UPDATE.sql
@@ -2,3 +2,8 @@
 UPDATE sakila.actor
 SET first_name = 'JULIES'
 WHERE first_name = 'JULIA';
+
+/* 2- Foi exigido que a categoria "Sci-Fi" seja alterada para "Science Fiction". */
+UPDATE sakila.category
+SET name = 'Science Fiction'
+WHERE name = 'Sci-Fi';
