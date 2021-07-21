e26988fc112ae258f0ba864a8b3002b97327fee2
Flavio Franco <flavio.win@gmail.com>
2021-07-21 20:10:57 -0300

    exercicio 2

--- DELETE.sql
+++ DELETE.sql
@@ -1,3 +1,17 @@
 -- 1- Exclua do banco de dados o ator com o nome de "KARL".
-SELECT actor_id FROM sakila.actor
+DELETE FROM sakila.film_actor
+WHERE actor_id = 12;
+
+DELETE FROM sakila.actor
 WHERE first_name = 'KARL';
+
+-- 2- Exclua do banco de dados os atores com o nome de "MATTHEW".
+SELECT * FROM sakila.actor
+WHERE first_name = 'MATTHEW';
+
+DELETE FROM sakila.film_actor
+WHERE actor_id IN (8, 103, 181);
+
+DELETE FROM sakila.actor
+WHERE first_name = 'MATTHEW';
+
