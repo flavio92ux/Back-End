c12030d086cd0f2b2d5562d62f2ad00a56a3b73d
Flavio Franco <flavio.win@gmail.com>
2021-07-21 09:39:59 -0300

    exercicio 5

--- Exercicios.sql
+++ Exercicios.sql
@@ -83,3 +83,7 @@ LIKE '%N%';
 SELECT * FROM PecasFornecedores.Fornecedores
 WHERE name LIKE '%LTDA%'
 ORDER BY name DESC;
+
+/* 5- Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código. */
+SELECT COUNT(*) FROM PecasFornecedores.Fornecedores
+WHERE name LIKE '%F%';
