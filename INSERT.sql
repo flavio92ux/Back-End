7f4a3d942f57c57ffbeaf5af31ec1f6b80eafcea
Flavio Franco <flavio.win@gmail.com>
2021-07-21 13:46:46 -0300

    exercicio 2

--- INSERT.sql
+++ INSERT.sql
@@ -1,4 +1,14 @@
 /* 1- Insira um novo funcionário na tabela sakila.staff . */
 INSERT INTO sakila.staff
   (first_name, last_name, address_id, email, store_id, username, password, active)
-VALUES('flavio', 'franco', 6, 'flavio.win@gmail.com', 1, 'flavio92ux', '123', 1);
+VALUES
+  ('flavio', 'franco', 6, 'flavio.win@gmail.com', 1, 'flavio92ux', '123', 1);
+
+/* 2- Insira dois funcionários novos em apenas uma query . */
+
+INSERT INTO sakila.staff
+  (first_name, last_name, address_id, email, store_id, username, password, active)
+VALUES
+  ('joana', 'tauber', 7, 'sjoanaesc@gmail.com', 1, 'joanete', '123', 1),
+  ('dudu', 'zinha', 8, 'hat@gmail.com', 1, 'gato', '123', 1);
+
