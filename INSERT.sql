ed2f3bb2b943266f0b296d42fff37ab872cb8a16
Flavio Franco <flavio.win@gmail.com>
2021-07-21 14:18:51 -0300

    exercicio 5

--- INSERT.sql
+++ INSERT.sql
@@ -11,3 +11,16 @@ VALUES
   ('joana', 'tauber', 7, 'sjoanaesc@gmail.com', 1, 'joanete', '123', 1),
   ('dudu', 'zinha', 8, 'hat@gmail.com', 1, 'gato', '123', 1);
 
+  /* 3- Selecione os cinco primeiros nomes e sobrenomes da tabela sakila.customer e cadastre essas pessoas como atores na tabela sakila.actor . */
+INSERT INTO sakila.actor (first_name, last_name)
+SELECT first_name, last_name FROM sakila.customer
+ORDER BY customer_id
+LIMIT 5;
+
+/* 4- Cadastre três categorias de uma vez só na tabela sakila.category . */
+INSERT INTO sakila.category (name)
+VALUES ('Comedia'), ('Acao'), ('Aventura');
+
+/* 5- Cadastre uma nova loja na tabela sakila.store . */
+INSERT INTO sakila.store (manager_staff_id, address_id)
+VALUES (3, 3);
