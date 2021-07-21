977a94a7eedf9206022c2233fad4aa59e6851a0f
Flavio Franco <flavio.win@gmail.com>
2021-07-21 10:05:42 -0300

    exercicio 7

--- Exercicios.sql
+++ Exercicios.sql
@@ -92,3 +92,9 @@ WHERE name LIKE '%F%';
 SELECT * FROM PecasFornecedores.Fornecimentos
 WHERE Preco BETWEEN 15 AND 40
 ORDER BY Preco;
+
+/* 7- Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019 . */
+SELECT * FROM PecasFornecedores.Vendas
+WHERE DATE(order_date)
+BETWEEN '2018-04-15'
+AND '2019-07-30';
