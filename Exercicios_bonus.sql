5e5ef02636d121ba5786957d12ebf20e680aa65c
Flavio Franco <flavio.win@gmail.com>
2021-07-21 10:25:35 -0300

    bonus 5

--- Exercicios_bonus.sql
+++ Exercicios_bonus.sql
@@ -88,6 +88,8 @@ INSERT INTO Scientists(SSN, Name)
   WHERE Code IN ('AeH3', 'Ast3', 'Che1');
 
   /* 5- Escreva uma query para exibir todas as informações de todos os projetos com mais de 500 horas. */
+  SELECT * FROM Scientists.Projects
+  WHERE Hours > 500;
 
   /* 6- Escreva uma query para exibir todas as informações de todos os projetos cujas horas sejam maiores que 250 e menores 800. */
 
