4ca46d0cf149393d5b6be9ba19e855619eb32cfa
Flavio Franco <flavio.win@gmail.com>
2021-07-21 13:43:07 -0300

    exemplo 1

---
+++ INSERT.sql
@@ -0,0 +1,4 @@
+/* 1- Insira um novo funcionário na tabela sakila.staff . */
+INSERT INTO sakila.staff
+  (first_name, last_name, address_id, email, store_id, username, password, active)
+VALUES('flavio', 'franco', 6, 'flavio.win@gmail.com', 1, 'flavio92ux', '123', 1);
