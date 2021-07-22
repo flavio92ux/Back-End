05be8f61f562125ee9d1c84a52e3bcc8f351fe79
Flavio Franco <flavio.win@gmail.com>
2021-07-22 10:37:52 -0300

    finalizando os exercicios bonus

--- EXERCICIOS.sql
+++ EXERCICIOS.sql
@@ -45,3 +45,42 @@ SELECT DISTINCT Project FROM AssignedTo;
 SELECT Name FROM Projects
 ORDER BY Hours DESC
 LIMIT 1;
+
+/* 13- Escreva uma query para exibir o nome do segundo projeto com menor quantidade de horas. */
+SELECT * FROM Projects
+ORDER BY Hours
+LIMIT 1
+OFFSET 1;
+
+/* 14- Escreva uma query para exibir todas as informações dos cinco projetos com a menor quantidade de horas. */
+SELECT * FROM Projects
+ORDER BY Hours
+LIMIT 5;
+
+/* 15- Escreva uma query que exiba a string "Existem Number cientistas na tabela Scientists.", em que Number se refira a quantidade de cientistas. */
+SELECT CONCAT('Existem ', COUNT(*), ' cientistas na tabela Scientistas' ) AS 'phrase' FROM Scientists;
+
+---------------------- BONUS -----------------------------
+
+/* 1- Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT  */
+SELECT Piece, Price FROM Provides
+WHERE Provider = 'RBT';
+
+/* 2- Escreve uma query para exibir todas as informações das cinco peças com os maiores preços. */
+SELECT * FROM Provides
+ORDER BY Price DESC
+LIMIT 5;
+
+/* 3- Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item. */
+SELECT Provider, Price FROM Provides
+ORDER BY Price DESC
+LIMIT 4
+OFFSET 3;
+
+/* 4- Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL . Ordene o resultado pelos preços das peças de forma decrescente. */
+SELECT * FROM Provides
+where Provider = 'HAL';
+
+/* 5- Escreva uma query para exibir por quantas empresas a peça 1 é provida. */
+SELECT COUNT(*) FROM Provides
+WHERE Piece = 1;
