a10ab290e3eb3cfb6de44c9c42cbe0435436dab0
Flavio Franco <flavio.win@gmail.com>
2021-07-24 13:19:36 -0300

    groupby

--- GroupBy.sql
+++ GroupBy.sql
@@ -7,3 +7,13 @@ SELECT store_id, active, COUNT(*) FROM sakila.customer
 GROUP BY store_id, active;
 
 /* 3- Monte uma query que exiba a média de duração por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor. */
+SELECT AVG(rental_duration), rating FROM sakila.film
+GROUP BY rating
+ORDER BY AVG(rental_duration) DESC;
+
+/* 4- Monte uma query para a tabela sakila.address que exiba o nome do distrito e a quantidade de endereços registrados nele. Os resultados devem ser ordenados da maior quantidade para a menor. */
+SELECT district, COUNT(*)
+AS 'address_amount'
+FROM sakila.address
+GROUP BY district
+ORDER BY COUNT(*) DESC;
