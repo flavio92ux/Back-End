825e32a4d1b2f913178efc699c5788436c34fcd2
Flavio Franco <flavio.win@gmail.com>
2021-07-24 13:08:23 -0300

    groupby

---
+++ GroupBy.sql
@@ -0,0 +1,9 @@
+/* 1- Monte uma query que exiba a quantidade de clientes cadastrados na tabela sakila.customer que estão ativos e a quantidade que estão inativos. */
+SELECT active, COUNT(*) FROM sakila.customer
+GROUP BY active;
+
+/* 2- Monte uma query para a tabela sakila.customer que exiba a quantidade de clientes ativos e inativos por loja. Os resultados devem conter o ID da loja , o status dos clientes (ativos ou inativos) e a quantidade de clientes por status */
+SELECT store_id, active, COUNT(*) FROM sakila.customer
+GROUP BY store_id, active;
+
+/* 3- Monte uma query que exiba a média de duração por classificação indicativa ( rating ) dos filmes cadastrados na tabela sakila.film . Os resultados devem ser agrupados pela classificação indicativa e ordenados da maior média para a menor. */
