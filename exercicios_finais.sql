de1d3cf96e232773fd9a8df46dd804077f07de50
Flavio Franco <flavio.win@gmail.com>
2021-07-24 14:13:13 -0300

    exercicio 6

--- exercicios_finais.sql
+++ exercicios_finais.sql
@@ -8,3 +8,15 @@ SELECT MAX(salary) - MIN(salary) AS 'difference' FROM hr.employees;
 SELECT job_id, AVG(salary) AS 'average_salary' from hr.employees
 GROUP BY job_id
 ORDER BY average_salary DESC;
+
+/* 4- Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias. */
+SELECT SUM(salary) AS 'payment_hiring' from hr.employees;
+
+/* 5- Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários.
+Todos os valores devem ser formatados para ter apenas duas casas decimais. */
+SELECT MAX(salary), MIN(salary), SUM(salary), ROUND(AVG(salary), 2) from hr.employees;
+
+/* 6- Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ). */
+SELECT COUNT(*) FROM hr.employees
+GROUP BY job_id
+HAVING job_id = 'IT_PROG';
