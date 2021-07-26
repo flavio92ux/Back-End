52dbd8c133659e85aec329e7fc993b75a454bec8
Flavio Franco <flavio.win@gmail.com>
2021-07-26 16:33:37 -0300

    exercicio 6

--- INNERJOIN.sql
+++ INNERJOIN.sql
@@ -51,3 +51,13 @@ WHERE
   c.active = 1
 GROUP BY c.first_name
 ORDER BY c.first_name DESC;
+
+/* 6- Monte uma query que exiba o nome , sobrenome e a média de valor ( amount ) paga aos funcionários no ano de 2006.
+Use as tabelas payment e staff . Os resultados devem estar agrupados pelo nome e sobrenome do funcionário. */
+SELECT s.first_name, s.last_name, AVG(p.amount)
+FROM
+  sakila.staff as s
+    INNER JOIN
+  sakila.payment as p ON s.staff_id = p.staff_id
+  WHERE YEAR(p.payment_date) = 2006
+  GROUP BY s.first_name, s.last_name;
