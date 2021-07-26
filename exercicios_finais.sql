42b160294b8d4664043c27b8aff81bb7e9c14424
Flavio Franco <flavio.win@gmail.com>
2021-07-26 09:22:30 -0300

    exercicio 14

--- exercicios_finais.sql
+++ exercicios_finais.sql
@@ -20,3 +20,36 @@ SELECT MAX(salary), MIN(salary), SUM(salary), ROUND(AVG(salary), 2) from hr.empl
 SELECT COUNT(*) FROM hr.employees
 GROUP BY job_id
 HAVING job_id = 'IT_PROG';
+
+/* 7- Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ) */
+SELECT job_id, SUM(salary) FROM hr.employees
+GROUP BY job_id;
+
+/* 8- Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária
+para cobrir a folha de pagamento das pessoas programadoras ( IT_PROG ). */
+SELECT job_id, SUM(salary) FROM hr.employees
+GROUP BY job_id
+HAVING job_id = 'IT_PROG';
+
+/* 9-  Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos,
+exceto das pessoas programadoras ( IT_PROG ) */
+SELECT job_id, AVG(salary) AS 'media_salario' FROM hr.employees
+GROUP BY job_id
+HAVING job_id <> 'IT_PROG'
+ORDER BY media_salario DESC;
+
+/* 10- Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos
+com mais de dez funcionários. Dica: agrupe pelo department_id */
+SELECT COUNT(*) AS "number_employees", AVG(salary) AS 'avg salary' FROM hr.employees
+GROUP BY department_id
+HAVING number_employees > 10;
+
+/* 11- Escreva uma query que atualize a coluna PHONE_NUMBER ,
+de modo que todos os telefones iniciados por 515 agora devem iniciar com 777  */
+UPDATE hr.employees
+SET phone_number = REPLACE(phone_number, '515', '777')
+WHERE phone_number LIKE '515%'; 
+
+/* 12- Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres. */
+SELECT * FROM hr.employees
+WHERE LENGTH(first_name > 7);
