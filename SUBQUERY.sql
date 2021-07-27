29a989b87084303a857d00901a8a67c7699776a1
Flavio Franco <flavio.win@gmail.com>
2021-07-27 10:04:32 -0300

    exists

--- SUBQUERY.sql
+++ SUBQUERY.sql
@@ -35,3 +35,18 @@ SELECT
         WHERE address.address_id = tabela_externa.address_id
     ) AS address
 FROM sakila.customer AS tabela_externa;
+
+
+----SUBQUERY VS JOIN-----
+SELECT
+    first_name,
+    (
+        SELECT address
+        FROM sakila.address
+        WHERE address.address_id = tabela_externa.address_id
+    ) AS address
+FROM sakila.customer AS tabela_externa;
+
+SELECT c.first_name, ad.address
+FROM sakila.customer c
+INNER JOIN sakila.address ad ON c.address_id = ad.address_id;
