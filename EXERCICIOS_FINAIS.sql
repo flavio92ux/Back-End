4f0ed64a9eb546b8e977bdbafdec9499193d8b42
Flavio Franco <flavio.win@gmail.com>
2021-07-27 18:05:22 -0300

    exercicio 6

--- EXERCICIOS_FINAIS.sql
+++ EXERCICIOS_FINAIS.sql
@@ -45,6 +45,22 @@ ORDER BY t.name;
 
 /* 6- Faça duas buscas, uma utilizando SUBQUERY e outra utilizando INNER JOIN , que retornem os títulos dos filmes que possuem
  avaliação maior que 7.5 */
+SELECT
+  title
+FROM
+  Movies
+WHERE id IN (SELECT movie_id FROM BoxOffice WHERE rating > 7.5);
+
+SELECT
+  title
+FROM
+  Movies m
+INNER JOIN
+  BoxOffice b ON b.movie_id = m.id
+WHERE b.rating > 7.5;
+
+
+
 
 
 
