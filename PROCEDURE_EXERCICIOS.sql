427c171ce11778d3c802ed775dfb28d46e9bd372
Flavio Franco <flavio.win@gmail.com>
2021-07-31 10:14:13 -0300

    finalizando

--- PROCEDURE_EXERCICIOS.sql
+++ PROCEDURE_EXERCICIOS.sql
@@ -46,3 +46,25 @@ END $$
 DELIMITER ;
 
 CALL showFilmsByCategories('ACTION');
+
+/* 3- Monte uma procedure que receba o email de um cliente como parâmetro de entrada e diga se o
+cliente está ou não ativo, através de um parâmetro de saída. */
+USE sakila;
+DELIMITER $$
+
+CREATE PROCEDURE verificaClienteAtivoPorEmail(IN eMail VARCHAR(100))
+BEGIN
+  SELECT
+    IF(active = 1, 'Ativo', 'Não ativo') AS 'Status'
+  FROM
+    staff
+  WHERE email = eMail;
+END $$
+
+DELIMITER ;
+
+CALL verificaClienteAtivoPorEmail('Mike.Hillyer@sakilastaff.com');
+
+SELECT * FROM staff;
+
+
