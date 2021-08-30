67bdae6cf7a9a545227b22c95bd422bd0bd9deaa
Flavio Franco <flavio.win@gmail.com>
2021-08-30 09:28:52 -0300

    exerc

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Exercícios 26.1 - Introdução ao Node.js
+
+Pacotes:
+
+npm i readline-sync
+const readline = require('readline-sync'); #permite perguntar ao usuario dados a serem inseridos.
--- scripts/sorteio.js
+++ scripts/sorteio.js
@@ -1,13 +1,35 @@
-/* Crie um "jogo de adivinhação" em que a pessoa ganha se acertar qual foi o número aleatório gerado */
+/* Crie um "jogo de adivinhação" em que a pessoa ganha se acertar qual foi o número aleatório gerado
+  -O script deve ser executado através do comando npm run sorteio
+  -Utilize o readline-sync para realizar input de dados.
+  -Armazene o script em sorteio.js
+  -O número gerado deve ser um inteiro entre 0 e 10.
+  -Caso a pessoa acerte o número, exiba na tela "Parabéns, número correto!".
+  -Caso a pessoa erre o número, exiba na tela "Opa, não foi dessa vez. O número era [número sorteado]".
+  -Ao final, pergunte se a pessoa deseja jogar novamente. Se sim, volte ao começo do script.
+*/
 const readline = require('readline-sync');
 
-const numero = readline.questionInt('Escolha um numero de 0 a 10: ');
+function logResultado(numero, resposta) {
+  if (numero === resposta) {
+    return console.log('')
+  }
 
-const random = parseInt(Math.random() * 10);
+  return console.log(`Opa, não foi dessa vez. O número era ${resposta}`);
+}
 
-if (numero === random) {
-  console.log('Acertou!');
-} else {
-  console.log('Errou!');
-  console.log(random);
-};
+function runGame() {
+  const resposta = parseInt(Math.random() * 10);
+  const numero = readline.questionInt('Escolha um numero de 0 a 10: ');
+
+  logResultado(numero, resposta);
+
+  const novamente = readline.question(
+    'Deseja jogar novamente? (Digite s para sim, e qualquer outra coisa para não)'
+  );
+
+  if (novamente !== 's') return console.log('OK, até a próxima!');
+
+  runGame();
+}
+
+runGame();
