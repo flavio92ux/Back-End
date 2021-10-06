67189292a39e8d4a48d51ac63376ab129479fdfc
Flavio Franco <flavio.win@gmail.com>
2021-10-06 20:21:21 -0300

    branch

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-const express = require('express');
+// ./index.js
 
-const app = express();
+/* "Converter" */
+const percentageGradesIntoLetters = ({ name, disciplines }) => ({
+  name,
+  disciplines: disciplines.map(({ name, grade }) => {
+    let letterGrade;
 
-const PORT = process.env.PORT || 3000;
-const MESSAGE = process.env.MESSAGE || 'Ola mundo!';
+    if (grade >= 0.9) letterGrade = 'A';
+    else if (grade >= 0.8) letterGrade = 'B';
+    else if (grade >= 0.7) letterGrade = 'C';
+    else if (grade >= 0.6) letterGrade = 'D';
+    else if (grade >= 0.1) letterGrade = 'E';
+    else letterGrade = 'F';
 
-app.get('/', (_req, res) => {
-  res.send(MESSAGE);
-});
+    return { name, grade, letterGrade };
+  })});
 
-app.listen(PORT, () => {
-  console.log(`Ouvindo na porta: ${PORT}`);
-});
+/* "Determinar" */
+const approvedStudents = ({ disciplines }) =>
+  disciplines.every(({ grade }) => grade > 0.7);
 
+/* "Atualizar" */
+const updateApprovalData = ({ name: studentName, disciplines }) => {
+  console.log(`A pessoa com nome ${studentName} foi aprovada!`);
+
+  disciplines.map(({ name, letterGrade }) =>
+    console.log(`${name}: ${letterGrade}`));
+};
+
+function setApproved(students) {
+  students
+    .map(percentageGradesIntoLetters)
+    .filter(approvedStudents)
+    .map(updateApprovalData);
+}
+
+/* Exemplo de execução */
+// ...
+
+/*
+  Não se esqueça que é necessário exportar ao final as
+  funções para que você possa testa-las de forma unitária
+*/
+module.exports = {
+  percentageGradesIntoLetters,
+  approvedStudents,
+  updateApprovalData,
+  setApproved,
+};
