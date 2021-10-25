a0abbfaef81ead6f78caa49b4d075c53bc154be8
Flavio Franco <flavio.win@gmail.com>
2021-10-24 22:13:29 -0300

    exercicio final

---
+++ refazendo exercicio final/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST',
+  }
+});
+
+app.use(express.static(__dirname + '/public'));
+
+require('./sockets/feed')(io);
+
+http.listen(3000, () => {
+  console.log('Ouvindo a porta 3000');
+});
